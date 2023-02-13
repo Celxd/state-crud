44cb77c1d991ffa35fc14945ad17bb3f430dac73
Haykal Hanif <98168247+Hykalseo@users.noreply.github.com>
2023-02-13 18:07:43 +0700

    Feat notif (firebase_options harus generate ulang)

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,37 @@ import 'package:state/crud.dart';
 import 'package:state/home.dart';
 import 'page2.dart';
 import 'loginPage.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
 
   runApp(const MyApp());
 }
